29f9288a46419c75d2375aa687766d7349b14e7a
Canuto <adrodriguezb99@gmail.com>
2023-02-21 11:11:04 -0500

    Funcion creada por Adrian

---
+++ funciones.js
@@ -0,0 +1,18 @@
+//Par e Impaar
+do {
+    var numero = prompt("Ingrese un número de valor Entero Positivo \nSi desea finalizar el programa ingrese la palabra salir")
+    alert(parImpar(numero))
+  
+    function parImpar(numero) {
+      if (Number.isSafeInteger(parseFloat(numero)) && parseInt(numero) >= 0) {
+        if (numero % 2 === 0) {
+          return "El número " + numero + " es par";
+        }
+        else { return "El número " + numero + " es impar"; }
+      } if (numero === "salir") {
+        return "adios";
+      }
+      return (numero + " No es un caracter valido, intente nuevamente");
+      
+    }
+  }while (numero !== "salir");ñ
