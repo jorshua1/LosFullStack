f1db03dc5cf9ec51e610856c7b64d22d3bccc92c
jorshua <jorshua321@gmail.com>
2023-03-14 11:59:40 -0500

    primer diseño maquetado

---
+++ javascript/this.js
@@ -0,0 +1,11 @@
+//! metodos del objeto "this"
+// let numero = Number(prompt())
+let numero =  13
+
+
+
+if(numero<1 && numero>12){
+    console.log("ingrese un numero valido de mes");
+}else{
+    console.log(numero);
+}
