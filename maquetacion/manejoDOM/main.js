51454eda6b020386f0595418b22a2cf28e53412c
Breinner <breinnerfarid12@gmail.com>
2023-03-29 11:49:28 -0500

    DOM

---
+++ maquetacion/manejoDOM/main.js
@@ -0,0 +1,38 @@
+// alert("Ingresando al archivo HTML");
+
+let impuntual = document.getElementById("Breinner");
+let  estudiante = document.getElementsByClassName("estudiante");
+let hambre = document.getElementsByTagName("p")
+let claseImpuntual = document.querySelector(".impuntual")
+let idhambre = document.querySelector("#hambre")
+let  estudiantes = document.querySelectorAll(".estudiante");
+console.log(impuntual);
+console.log(estudiante);
+console.log(estudiante.Breinner);
+console.log(hambre);
+console.log(claseImpuntual)
+console.log(idhambre)
+console.log(estudiantes);
+
+for(let i =0; i < estudiantes.length; i++){
+if(estudiantes[i].className.includes("impuntual")){
+estudiante[i].style.backgroundColor = "#fffc30";
+estudiante[i].style.color = "#000";
+estudiante[i].style.border = "4px solid black";
+}else{
+
+}
+estudiante[i].style.color = "#0ff";
+
+
+}
+idhambre.style.backgroundColor = "#fffc30";
+idhambre.style.width = "250px";
+idhambre.style.padding = "25px";
+idhambre.style.border = "4px solid black";
+idhambre.style.borderRadius = "25px";
+idhambre.style.color = "#000";
+
+// const body = document.getElementsByTagName("body")[0];
+// console.log(body);
+document.getElementsByTagName("body")[0].style.fontFamily="Verdana, sans-serif";
