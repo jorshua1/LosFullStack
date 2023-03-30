0c30c91562ecbb41ff6ccb43d7c29f2f79ece111
jenifer <lizarazojenifer2@gmail.com>
2023-03-30 11:47:31 -0500

    dom

--- maquetacion/manejoDom/main.js
+++ maquetacion/manejoDom/main.js
@@ -5,12 +5,12 @@ let claseImpuntual = document.querySelector(".impuntual");
 let idHambre = document.querySelector("#hambre");
 let estudiante = document.querySelectorAll(".estudiante");
 
-console.log(impuntual);
-console.log(estudiantes);
-console.log(estudiantes.breinner);
-console.log(hambre);
-console.log(claseImpuntual);
-console.log(idHambre);
+//console.log(impuntual);
+//console.log(estudiantes);
+//console.log(estudiantes.breinner);
+//console.log(hambre);
+//console.log(claseImpuntual);
+//console.log(idHambre);
 
 impuntual.style.backgroundColor = "#ffc303";
 impuntual.style.border = " 4px solid black";
@@ -29,3 +29,25 @@ for (i = 0; i < estudiantes.length; i++) {
     estudiantes[i].style.color = "#ffc303";
   }
 }
+
+let listaTareas = document.getElementById("listaTareas");
+let tareas = [
+  "despertar",
+  "comer",
+  "bañarse",
+  "trabajar",
+  "almorzar",
+  "trabajar",
+  "comer",
+  "dormir",
+];
+for(let i=0; i<tareas.length; i++){
+  let tarea = document.createElement("li");
+  let texto = tareas[i];
+  tarea.innerHTML=texto;
+  listaTareas.append(tarea);
+  tarea.setAttribute("class","tarea");
+}
+
+console.log(listaTareas.innerText);
+idHambre.remove()
