4b534ae2518c9d6a2a9d6bf4e8e6972dc12f9c16
Breinner <breinnerfarid12@gmail.com>
2023-03-30 11:36:39 -0500

    holo

--- maquetacion/manejoDOM/main.js
+++ maquetacion/manejoDOM/main.js
@@ -1,30 +1,27 @@
 // alert("Ingresando al archivo HTML");
 
 let impuntual = document.getElementById("Breinner");
-let  estudiante = document.getElementsByClassName("estudiante");
-let hambre = document.getElementsByTagName("p")
-let claseImpuntual = document.querySelector(".impuntual")
-let idhambre = document.querySelector("#hambre")
-let  estudiantes = document.querySelectorAll(".estudiante");
+let estudiante = document.getElementsByClassName("estudiante");
+let hambre = document.getElementsByTagName("p");
+let claseImpuntual = document.querySelector(".impuntual");
+let idhambre = document.querySelector("#hambre");
+let estudiantes = document.querySelectorAll(".estudiante");
 console.log(impuntual);
 console.log(estudiante);
 console.log(estudiante.Breinner);
 console.log(hambre);
-console.log(claseImpuntual)
-console.log(idhambre)
+console.log(claseImpuntual);
+console.log(idhambre);
 console.log(estudiantes);
 
-for(let i =0; i < estudiantes.length; i++){
-if(estudiantes[i].className.includes("impuntual")){
-estudiante[i].style.backgroundColor = "#fffc30";
-estudiante[i].style.color = "#000";
-estudiante[i].style.border = "4px solid black";
-}else{
-
-}
-estudiante[i].style.color = "#0ff";
-
-
+for (let i = 0; i < estudiantes.length; i++) {
+  if (estudiantes[i].className.includes("impuntual")) {
+    estudiante[i].style.backgroundColor = "#fffc30";
+    estudiante[i].style.color = "#000";
+    estudiante[i].style.border = "4px solid black";
+  } else {
+  }
+  estudiante[i].style.color = "#0ff";
 }
 idhambre.style.backgroundColor = "#fffc30";
 idhambre.style.width = "250px";
@@ -35,4 +32,29 @@ idhambre.style.color = "#000";
 
 // const body = document.getElementsByTagName("body")[0];
 // console.log(body);
-document.getElementsByTagName("body")[0].style.fontFamily="Verdana, sans-serif";
+document.getElementsByTagName("body")[0].style.fontFamily =
+  "Verdana, sans-serif";
+
+// let listaTareas = document.getElementById("listaTareas");
+let tarea = document.createElement("li");
+let tareas = [
+  "Despertar",
+  "cepillarese",
+  "comer",
+  "bañarse",
+  "trabajar",
+  "almorzar",
+  "trabajar",
+  "comer",
+  "dormir"
+];
+
+
+for (let i = 0; i < tareas.length; i++) {
+    let tarea = document.createElement("li"); //crear un nuevo elemento en cada ciclo 
+    tarea.innerText = tareas[i];
+    listaTareas.appendChild(tarea);
+    tarea.setAttribute("class", "tarea")
+  }
+
+idhambre.remove();
