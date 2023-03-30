1fbf5ede2cdc3ff790c169ee6c892f1c37e9bb8f
gabriela <gabsantamaria19@gmail.com>
2023-03-30 11:47:23 -0500

    melito

--- maquetacion/manejoDOM/main.js
+++ maquetacion/manejoDOM/main.js
@@ -1,18 +1,16 @@
 // alert("Iniciando html");
 
-let impuntual = document.getElementById("Jorshua")
+let impuntual = document.getElementById("Jorshua");
 
-let estudiante = document.getElementsByClassName("estudiante")
+let estudiante = document.getElementsByClassName("estudiante");
 
-let hambre = document.getElementsByTagName("p")
+let hambre = document.getElementsByTagName("p");
 
-let claseImpuntual = document.querySelector(".impuntual")
+let claseImpuntual = document.querySelector(".impuntual");
 
-let idHambre = document.querySelector("#hambre")
+let idHambre = document.querySelector("#hambre");
 
-let estudiantes = document.querySelectorAll(".estudiante")
-
-
+let estudiantes = document.querySelectorAll(".estudiante");
 
 console.log(impuntual);
 console.log(estudiante);
@@ -22,28 +20,49 @@ console.log(claseImpuntual);
 console.log(idHambre);
 console.log(estudiantes);
 
-
 // impuntual.style.backgroundColor= "#ffc300";
 
 idHambre.style.backgroundColor = "#ffc300";
 idHambre.style.width = "250px";
-idHambre.style.padding= "20px";
-idHambre.style.border= "4px solid black";
-idHambre.style.borderRadius= "25px";
-idHambre.style.color= "black"
-
-
+idHambre.style.padding = "20px";
+idHambre.style.border = "4px solid black";
+idHambre.style.borderRadius = "25px";
+idHambre.style.color = "black";
 
 for (let i = 0; i < estudiantes.length; i++) {
-         if (estudiantes[i].className.includes("impuntual")){
-          estudiante[i].style.backgroundColor = "#ffc300";
-          estudiante[i].style.border= "4px solid black";
-          estudiante[i].style.color= "black"
-         }else{
-          estudiantes[i].style.color="#ffc300"
-         }
+  if (estudiantes[i].className.includes("impuntual")) {
+    estudiante[i].style.backgroundColor = "#ffc300";
+    estudiante[i].style.border = "4px solid black";
+    estudiante[i].style.color = "black";
+  } else {
+    estudiantes[i].style.color = "#ffc300";
+  }
 }
 
+document.getElementsByTagName("body")[0].style.fontFamily =
+  "Verdana, sans-serif";
 
-document.getElementsByTagName("body")[0].style.fontFamily="Verdana, sans-serif"
+let listaTareas = document.getElementById("listaTareas");
+let tarea = document.createElement("li");
 
+let tareas = [
+  "despertarse",
+  "cepillarse",
+  "comer",
+  "trabajar",
+  "almorzar",
+  "trabajar",
+  "comer",
+  "dormir",
+];
+
+
+for (let i = 0; i < tareas.length; i++) {
+  let tarea = document.createElement("li");
+  let texto = tareas[i];
+  tarea.innerHTML = texto;
+  listaTareas.append(tarea);
+  tarea.setAttribute("class", "tarea")
+}
+
+idHambre.remove()
