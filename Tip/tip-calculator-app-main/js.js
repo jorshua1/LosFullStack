a77b1c7766e5d1c6b6336caf1550cfee38891f5e
Nicolas Cadena <nicolascadena18@gmail.com>
2023-04-08 17:58:00 -0500

    Nicolas

--- Tip/tip-calculator-app-main/js.js
+++ Tip/tip-calculator-app-main/js.js
@@ -6,8 +6,21 @@ const cinco = () => {
   let personatip = tip / persona;
   let total = personacuenta + personatip;
 
-  document.getElementById("tips").value = ("$" + personatip);
-  document.getElementById("total").value = ("$" + total);
+  document.getElementById("tips").value = "$" + personatip;
+  document.getElementById("total").value = "$" + total;
+
+   let boton1 = document.querySelector("#boton1");
+   function cambiarEstilo() {
+     boton1.style.backgroundColor = "#9fe8df";
+     boton1.style.color = "hsl(183, 100%, 15%)";
+   }
+   function Normal() {
+     boton1.style.backgroundColor = "hsl(183, 100%, 15%)";
+     boton1.style.color = "hsl(0, 0%, 100%)";
+   }
+ 
+   boton1.addEventListener("click", cambiarEstilo);
+   boton1.addEventListener("mouseleave", Normal);
 };
 
 const diez = () => {
@@ -18,8 +31,21 @@ const diez = () => {
   let personatip = tip / persona;
   let total = personacuenta + personatip;
 
-  document.getElementById("tips").value = ("$" + personatip);
-  document.getElementById("total").value = ("$" + total);
+  document.getElementById("tips").value = "$" + personatip;
+  document.getElementById("total").value = "$" + total;
+
+  let boton2 = document.querySelector("#boton2");
+  function cambiarEstilo() {
+    boton2.style.backgroundColor = "#9fe8df";
+    boton2.style.color = "hsl(183, 100%, 15%)";
+  }
+  function Normal() {
+    boton2.style.backgroundColor = "hsl(183, 100%, 15%)";
+    boton2.style.color = "hsl(0, 0%, 100%)";
+  }
+
+  boton2.addEventListener("click", cambiarEstilo);
+  boton2.addEventListener("mouseleave", Normal);
 };
 const quince = () => {
   let valor = document.querySelector(".plata").value;
@@ -29,8 +55,20 @@ const quince = () => {
   let personatip = tip / persona;
   let total = personacuenta + personatip;
 
-document.getElementById("tips").value = ("$" + personatip);
-  document.getElementById("total").value = ("$" + total);
+  document.getElementById("tips").value = "$" + personatip;
+  document.getElementById("total").value = "$" + total;
+  let boton3 = document.querySelector("#boton3");
+  function cambiarEstilo() {
+    boton3.style.backgroundColor = "#9fe8df";
+    boton3.style.color = "hsl(183, 100%, 15%)";
+  }
+  function Normal() {
+    boton3.style.backgroundColor = "hsl(183, 100%, 15%)";
+    boton3.style.color = "hsl(0, 0%, 100%)";
+  }
+
+  boton3.addEventListener("click", cambiarEstilo);
+  boton3.addEventListener("mouseleave", Normal);
 };
 const veinticinco = () => {
   let valor = document.querySelector(".plata").value;
@@ -40,8 +78,20 @@ const veinticinco = () => {
   let personatip = tip / persona;
   let total = personacuenta + personatip;
 
-  document.getElementById("tips").value = ("$" + personatip);
-  document.getElementById("total").value = ("$" + total);
+  document.getElementById("tips").value = "$" + personatip;
+  document.getElementById("total").value = "$" + total;
+  let boton4 = document.querySelector("#boton4");
+  function cambiarEstilo() {
+    boton4.style.backgroundColor = "#9fe8df";
+    boton4.style.color = "hsl(183, 100%, 15%)";
+  }
+  function Normal() {
+    boton4.style.backgroundColor = "hsl(183, 100%, 15%)";
+    boton4.style.color = "hsl(0, 0%, 100%)";
+  }
+
+  boton4.addEventListener("click", cambiarEstilo);
+  boton4.addEventListener("mouseleave", Normal);
 };
 const cincuenta = () => {
   let valor = document.querySelector(".plata").value;
@@ -51,14 +101,42 @@ const cincuenta = () => {
   let personatip = tip / persona;
   let total = personacuenta + personatip;
 
-  document.getElementById("tips").value = ("$" + personatip);
-  document.getElementById("total").value = ("$" + total);
+  document.getElementById("tips").value = "$" + personatip;
+  document.getElementById("total").value = "$" + total;
+  let boton5 = document.querySelector("#boton5");
+  function cambiarEstilo() {
+    boton5.style.backgroundColor = "#9fe8df";
+    boton5.style.color = "hsl(183, 100%, 15%)";
+  }
+  function Normal() {
+    boton5.style.backgroundColor = "hsl(183, 100%, 15%)";
+    boton5.style.color = "hsl(0, 0%, 100%)";
+  }
+
+  boton5.addEventListener("click", cambiarEstilo);
+  boton5.addEventListener("mouseleave", Normal);
+};
+
+const custom = () => {
+  let boton6 = document.querySelector("#boton6");
+  function cambiarEstilo() {
+    boton6.style.backgroundColor = "#9fe8df";
+    boton6.style.color = "hsl(183, 100%, 15%)";
+  }
+  function Normal() {
+    boton6.style.backgroundColor = "hsl(183, 100%, 15%)";
+    boton6.style.color = "hsl(0, 0%, 100%)";
+  }
+
+  boton6.addEventListener("click", cambiarEstilo);
+  boton6.addEventListener("mouseleave", Normal);
+
 };
 
 
 const clean = () => {
-    document.querySelector(".plata").value = ("");
-    document.querySelector(".persona").value = ("");
-    document.getElementById("tips").value =("$0");
-    document.getElementById("total").value =("$0");
-}
+  document.querySelector(".plata").value = "";
+  document.querySelector(".persona").value = "";
+  document.querySelector("#tips").value = "$0";
+  document.querySelector("#total").value = "$0";
+};
