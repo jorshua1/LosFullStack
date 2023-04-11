65eca4290a90aea40a09ed72d17719e5d03a0a0b
Breinner <breinnerfarid12@gmail.com>
2023-04-11 11:36:03 -0500

    js

---
+++ maquetacion/frontMentor/cuartoDesafio/porsi.js
@@ -0,0 +1,7 @@
+let boton1 = document.querySelector("#boton1")
+let boton2 = document.querySelector("#boton2")
+let boton3 = document.querySelector("#boton3")
+
+let procedimiento = (porcentaje, valor)=> {
+  let calculoTip = (valor * porcentaje)
+};
