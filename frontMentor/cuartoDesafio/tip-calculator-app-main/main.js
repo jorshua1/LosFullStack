673b0ddf191936bd3e17881bc6e945393bc06e87
Jorshua <jorshua321@gmail.com>
2023-04-17 11:34:46 -0500

    fix gab

--- frontMentor/cuartoDesafio/tip-calculator-app-main/main.js
+++ frontMentor/cuartoDesafio/tip-calculator-app-main/main.js
@@ -1,24 +1,20 @@
-console.log("xd");
 let boton1 = document.querySelector('#botn1');
 let boton2 = document.querySelector('#botn2');
 let boton3 = document.querySelector('#botn3');
 let boton4 = document.querySelector('#botn4');
 let boton5 = document.querySelector('#botn5');
-let reseteado = document.querySelector('#reset')
-console.log(boton1);
+let reseteado = document.querySelector('#reset');
 
 let procedimiento = (porcentaje, valor) => {
   let personas = Number(document.querySelector("#cantipeople").value)
-  console.log(typeof porcentaje);
-  console.log(valor);
   let calculoTip = ((valor * porcentaje) / 100) /personas;
   return calculoTip;
 };
 
-function actualizaCampos(valorTotal, valorTip){
-    document.getElementById("total").value = "$" + valorTotal.toFixed(2);
-    document.getElementById("tip").value = "$" + valorTip.toFixed(2); 
-}
+// function actualizaCampos(valorTotal, valorTip){
+//     document.getElementById("total").value = "$" + valorTotal.toFixed(2);
+//     document.getElementById("tip").value = "$" + valorTip.toFixed(2); 
+// }
 
 function calculoPropina(porcentaje){
     let valor = parseFloat(document.querySelector("#valor").value);
@@ -32,17 +28,18 @@ function calculoPropina(porcentaje){
     }else{
         let tip = procedimiento(porcentaje, valor);
         let total = (tip + (valor/personas));
-        actualizaCampos(total, tip);
-        error.removeAttribute("class", "alerthidden")
-        error.setAttribute("class", "alertShow")
+        // actualizaCampos(total, tip);
+        // error.removeAttribute("class", "alerthidden")
+        // error.setAttribute("class", "alertShow")
+        console.log(tip, total)
     }
 }
 
-boton1.addEventListener("click", () => procedimiento(5, 100000));
-boton2.addEventListener("click", () => procedimiento(10));
-boton3.addEventListener("click", () => procedimiento(15));
-boton4.addEventListener("click", () => procedimiento(25));
-boton5.addEventListener("click", () => procedimiento(50));
+boton1.addEventListener("click", () => calculoPropina(5));
+boton2.addEventListener("click", () => calculoPropina(10));
+boton3.addEventListener("click", () => calculoPropina(15));
+boton4.addEventListener("click", () => calculoPropina(25));
+boton5.addEventListener("click", () => calculoPropina(50));
 // reseteado.addEventListener("click", () => calculoPropina(parseInt(reseteado.value)));
 
 function clean() {
@@ -52,6 +49,6 @@ function clean() {
     document.getElementById("tip").value = "$0.00";
   }
 
-  document.getElementById("formCalculadora").addEventListener("submit", (e) => {
-    e.preventDefault();
-  });
+  // document.getElementById("formCalculadora").addEventListener("submit", (e) => {
+  //   e.preventDefault();
+  // });
