498e8408d26eb9034e9a2d2d766243ad9f221cee
Yuli <yulikatelipez208@gmail.com>
2023-04-19 12:03:14 -0500

    tallercinco

---
+++ frontMentor/quintoDesafio/ejercio5.js
@@ -0,0 +1,10 @@
+let boton1 = document.getElementById("boton1");
+let boton2 = document.getElementById("boton2");
+
+boton1.addEventListener('click', function(abrir){
+    contenerdo3(abrir)
+})
+
+boton2.addEventListener('click',function (cerrar){
+    alert("le has dado click al boton")
+});
